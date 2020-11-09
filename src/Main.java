af484337f4a7cb49b5dae9f033c0749b78eb1b5f
Luke Schweickart <lukeschweickart@gmail.com>
2020-11-09 09:50:58 -0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,48 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    // Problem 1
+        System.out.println("Problem #1:");
+        int[] problem1 = {11,12,13,14,15,16,17,18,19,20};
+        for (int i=0; i<problem1.length; i++) {
+            if (i % 2 == 0) {
+                problem1[i] = 0;
+            }
+        }
+        for (int i=0; i< problem1.length; i++) {
+            System.out.print(problem1[i] + " ");
+        }
+
+        // Problem 2
+        System.out.println("\n\nProblem #2:");
+        String[] problem2 = {"first", "second", "third", "fourth", "fifth", "sixth"};
+
+        String temp2 = problem2[problem2.length-1];
+        for(int i = problem2.length-1; i > 0; i--) {
+            problem2[i] = problem2[i-1];
+        }
+        problem2[0] = temp2;
+
+        for (int i=0; i< problem2.length; i++) {
+            System.out.print(problem2[i] + " ");
+        }
+
+        // Problem 3
+        System.out.println("\n\nProblem #3:");
+        int[] problem3 = {11,-12,-13,14,-15,16,17,-18,19,-20};
+        int prob3sum = 0;
+        for (int i=0; i<problem3.length; i++) {
+            if (problem3[i] < 0) {
+                prob3sum += problem3[i];
+            }
+        }
+        for (int i=0; i< problem3.length; i++) {
+            System.out.print(problem3[i] + " ");
+        }
+        System.out.println("\nSum: "+ prob3sum);
+
+        // Problem 4
+        System.out.println("\n\nProblem #4:");
+
+    }
+}
